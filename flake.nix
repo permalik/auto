ebf89207c6397c50cd04fdc383bd1854cdd43e2f
permalik <permalik@protonmail.com>
2025-03-26 15:14:13 -0500

    feat: dynamic system config

--- flake.nix
+++ flake.nix
@@ -5,36 +5,45 @@
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
   };
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs {
-      config = {
-        allowUnfree = false;
-      };
+  outputs = { self, nixpkgs }:
+    let
+        systems = [ "x86_64-linux" "aarch64-darwin" ];
+        forAllSystems = f: builtins.listToAttrs (map (system: {
+            name = system;
+            value = f system;
+        }) systems);
+    in
+    {
+        devShells = forAllSystems (system: let
+            pkgs = import nixpkgs {
+                inherit system;
+                config.allowUnfree = false;
+            };
+
+            myPython = pkgs.python312;
+            pythonWithPkgs = myPython.withPackages (pythonPkgs: with pythonPkgs; [
+                pip
+                virtualenvwrapper
+            ]);
+
+        shell = pkgs.mkShell {
+            buildInputs = [
+                pythonWithPkgs
+                pkgs.readline
+            ];
+
+            shellHook = ''
+                VENV=.venv
+                if [ ! -d $VENV ]; then
+                    virtualenv $VENV
+                fi
+                source ./$VENV/bin/activate
+                echo "Python virtual environment activated."
+            '';
+        };
+    in {
+        default = shell;
+    });
     };
-
-    myPython = pkgs.python312;
-    pythonWithPkgs = myPython.withPackages (pythonPkgs: with pythonPkgs; [
-      pip
-      virtualenvwrapper
-    ]);
-
-    shell = pkgs.mkShell {
-      buildInputs = [
-        pythonWithPkgs
-        pkgs.readline
-      ];
-
-      shellHook = ''
-        VENV=.venv
-        if [ ! -d $VENV ]; then
-          virtualenv $VENV
-        fi
-        source ./$VENV/bin/activate
-        echo "Python virtual environment activated."
-      '';
-    };
-  in {
-    devShell = shell;
-  };
 }
 
