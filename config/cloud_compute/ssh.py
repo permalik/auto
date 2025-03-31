8f522e14d598c01426d2e1f21d1b3e735d55419d
permalik <permalik@protonmail.com>
2025-03-30 23:43:40 -0500

    feat: impl ssh.py

---
+++ config/cloud_compute/ssh.py
@@ -0,0 +1,8 @@
+import subprocess
+
+# sudo apt install isc-dhcp-server
+# virsh domiflist <vm-name>
+# ip a
+# ifconfig -a
+# sudo dhclient eth0  # Replace `eth0` with your interface name
+# etc/netplan/cloud-init.yaml
