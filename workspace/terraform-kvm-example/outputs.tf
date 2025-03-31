fa88b396e0cd27ded7903f5ad9cc29f5ade8cb7f
permalik <permalik@protonmail.com>
2025-03-31 05:09:27 +0000

    feat: init workspace

---
+++ workspace/terraform-kvm-example/outputs.tf
@@ -0,0 +1,6 @@
+output "ip" {
+	value = libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]
+}
+output "url" {
+	value = "http://${libvirt_domain.domain-ubuntu.network_interface[0].addresses[0]}"
+}
---
+++ workspace/terraform-kvm-example/variables.tf
@@ -0,0 +1,24 @@
+variable "libvirt_disk_path" {
+	description = "path for libvirt pool"
+	default = "/opt/kvm/pool1"
+}
+
+variable "ubuntu_24_img_url" {
+	description = "ubuntu 24.04 image"
+	default = "http://cloud-images.ubuntu.com/releases/noble/release-20250327/ubuntu-24.04-server-cloudimg-amd64.img"
+}
+
+variable "vm_hostname" {
+	description = "vm hostname"
+	default = "terraform-kvm-ansible"
+}
+
+variable "ssh_username" {
+	description = "the ssh user"
+	default = "ubuntu"
+}
+
+variable "ssh_private_key" {
+	description = "the private key"
+	default = "~/.ssh/id_rsa"
+}
