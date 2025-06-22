0b45559fbf16894ab01ee1d42014c89a0f12be9c
permalik <permalik@protonmail.com>
2025-06-22 13:04:31 -0500

    feat: yyy-mac init/dstr

---
+++ scripts/dstr/dstr-yyyoink-mac.sh
@@ -0,0 +1,55 @@
+#!/bin/zsh
+
+YYY_DIR="/Users/tymalik/Docs/Git/SunsplitStudio/yyyoink"
+
+REPOS=(
+    yyyoink-apple
+    yyyoink-web-frontend
+    yyyoink-web-backend
+    yyyoink-core
+    yyyoink-portal-frontend
+    yyyoink-portal-backend
+)
+
+cd "$YYY_DIR" || exit 1
+
+for repo in "${REPOS[@]}"; do
+    if [ -d "$repo/.git" ]; then
+        echo "Checking $repo"
+        cd "$repo" || continue
+
+        git status
+
+        if ! git diff --quiet || ! git diff --cached --quiet; then
+            echo "Found Changes in $repo: Adding, Committing, and Pushing.."
+            git add .
+            git commit -m "Auto commit before deletion." || echo "Nothing to commit."
+            git push
+        fi
+
+        LOCAL_HASH=$(git rev-parse @)
+        REMOTE_HASH=$(git rev-parse @{u})
+        BASE_HASH=$(git merge-base @ @{u})
+
+        # if [ "$LOCAL_HASH" = "$REMOTE_HASH" ]; then
+        #     echo "Local branch is fully pushed."
+        # elif [ "$LOCAL_HASH" = "$BASE_HASH" ]; then
+        #     echo "Local is behind remote. Need to pull."
+        # elif [ "$REMOTE_HASH" = "$BASE_HASH" ]; then
+        #     echo "Local is ahead of remote. Need to push."
+        # else
+        #     echo "Local and remote have diverged."
+        # fi
+
+        if [[ "$LOCAL_HASH" = "$REMOTE_HASH" ]]; then
+            echo "Deletion Safe: $repo"
+            cd "$YYY_DIR"
+            rm -rf "$repo"
+        else
+            echo "Unsafe Warning: $repo has unpushed commits."
+            cd "$YYY_DIR"
+        fi
+    else
+        echo "Cleanup Notice: $repo is not a git repo."
+    fi
+done
---
+++ scripts/init/init-yyyoink-mac.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+cd /Users/tymalik/Docs/Git/SunsplitStudio/yyyoink
+
+git clone git@github.com:sunsplitstudio/yyyoink-apple.git
+git clone git@github.com:sunsplitstudio/yyyoink-web-frontend.git
+git clone git@github.com:sunsplitstudio/yyyoink-web-backend.git
+git clone git@github.com:sunsplitstudio/yyyoink-core.git
+git clone git@github.com:sunsplitstudio/yyyoink-portal-frontend.git
+git clone git@github.com:sunsplitstudio/yyyoink-portal-backend.git
