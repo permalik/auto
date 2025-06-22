8b489357995170a301c8a0992d89dc32e674dd37
permalik <permalik@protonmail.com>
2025-06-22 13:28:51 -0500

    feat: yyy init-dstr

---
+++ scripts/init/init-yyyoink-win.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+cd /c/Users/perma/Docs/Git/project/yyyoink
+
+git clone git@github.com:sunsplitstudio/yyyoink-avalonia.git
+git clone git@github.com:sunsplitstudio/yyyoink-android.git
